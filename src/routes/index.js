be0e3d32cfc7208bfdc7fcc048cd4e0fe855e916
Lucas Fernandes <lucas12310@hotmail.com>
2024-11-17 14:13:18 -0300

    vercel file

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,8 +12,13 @@ const routes = (app) => {
 
     // Configurando a documentação do Swagger UI
     app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(getSwaggerOptions()), {
-        customCssUrl: ["https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js" ],
+        customCssUrl: [
+            "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css"
+          ],
+          customJsUrl: [
+            "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js",
+            "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js"
+          ],
         customSiteTitle: "API Barbeiro", // Personalizando o título da página de documentação
     }));
 
