b12a667b0af9cbd76490f5adf6a66cdab09211b0
Lucas Fernandes <lucas12310@hotmail.com>
2024-11-17 13:51:10 -0300

    vercel file

--- src/app.js
+++ src/app.js
@@ -4,13 +4,7 @@ import routes from "../src/routes/index.js";
 const app = express()
 
 import cors from "cors";
-
-app.use(cors({
-    origin: '*', // Permitir requisições da aplicação rodando em localhost:3000
-    methods: ['GET', 'POST', 'PUT', 'PATCH','DELETE', 'OPTIONS'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization', "application/json"], // Cabeçalhos permitidos
-    credentials: true // Permitir envio de cookies ou headers de autenticação
-}));
+app.use(cors());
 
 app.use(express.json())
 routes(app);
