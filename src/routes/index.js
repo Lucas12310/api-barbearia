aa2049eda95f4116933058a89535b9b99fd47648
Lucas Fernandes <lucas12310@hotmail.com>
2024-11-17 13:33:01 -0300

    vercel file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,31 @@
 import express from "express";
-import barbeiro from "../routes/barbeiroRouter.js"
-//imports swagger
+import barbeiro from "../routes/barbeiroRouter.js";
+// Imports do Swagger
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
 import getSwaggerOptions from "../docs/config/head.js";
 
 const routes = (app) => {
-    // Configurando a documentação da Swagger UI para ser servida diretamente em '/'
-    app.get("/",(req, res) => {
-		res.status(200).redirect("docs"); // redirecionando para documentação
-	});
+    // Redirecionando para a documentação (agora corrigido para funcionar em qualquer ambiente)
+    app.get("/", (req, res) => {
+        res.status(200).redirect("/docs"); // Redirecionando para a rota '/docs' com a barra inicial
+    });
 
-	app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(getSwaggerOptions()), {
-		customCssUrl: [ "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css" ]
-	}));
+    // Configurando a documentação do Swagger UI
+    app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(getSwaggerOptions()), {
+        customCssUrl: [
+            "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css" // Usando o CSS do Swagger diretamente do CDN
+        ],
+        customSiteTitle: "API Barbeiro", // Personalizando o título da página de documentação
+    }));
 
-
+    // Configurando as rotas da aplicação (aqui estamos assumindo que `barbeiro` é a sua rota principal)
     app.use(
         express.json(),
         express.text(),
-        barbeiro,
+        barbeiro, // Rota do barbeiro, onde você tem suas APIs
     );
-
-
 };
 
 export default routes;
+
